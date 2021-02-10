93d93408d7e98dbb3577971dd69dd139281c643a
Vanessa Flores <floresv26@gmail.com>
2021-02-10 17:35:14 -0600

    Add pickerView to the ViewController

--- CustomPickerView/Screens/ViewController.swift
+++ CustomPickerView/Screens/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    let pickerView = UIPickerView(frame: .zero)
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        configurePickerView()
     }
 
-
+    private func configurePickerView() {
+        view.addSubview(pickerView)
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pickerView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            pickerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            pickerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            pickerView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
 }
 
